4ac8f0753fb6a21c989c8ad628c5acfd21155558
Abhimannu Singh Kunwar <anmolkunwar07@gmail.com>
2026-03-09 22:36:15 +0545

    feat: proper implementation of zod and routes

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import express, { Application, Request, Response } from "express";
+import authRoutes from "./routes/auth.routes";
 
 const app: Application = express();
+
+app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
