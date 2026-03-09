459bb602aed478dbcaa75699fd88e2c552ce991a
Abhimannu Singh Kunwar <anmolkunwar07@gmail.com>
2026-03-09 22:15:00 +0545

    feat: login user backend

--- middlewares/userController.ts
+++ middlewares/userController.ts
@@ -34,3 +34,37 @@ export const createUser = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      data: { user },
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Error processing request",
+      error: error instanceof Error ? error.message : error,
+    });
+  }
+};
